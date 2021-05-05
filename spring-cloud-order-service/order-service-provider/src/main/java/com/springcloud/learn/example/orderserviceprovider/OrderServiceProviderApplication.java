8c37b9a39e7a1c576c0d28db13dbc9ebc19841c5
zhengjianwei <yaoming.li@taojiji.com>
2021-05-05 09:46:13 +0800

    添加核心异常处理代码

--- spring-cloud-order-service/order-service-provider/src/main/java/com/springcloud/learn/example/orderserviceprovider/OrderServiceProviderApplication.java
+++ spring-cloud-order-service/order-service-provider/src/main/java/com/springcloud/learn/example/orderserviceprovider/OrderServiceProviderApplication.java
@@ -2,10 +2,7 @@ package com.springcloud.learn.example.orderserviceprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 
-@ComponentScan(basePackages = "com.springcloud.learn.example")
 @SpringBootApplication
 public class OrderServiceProviderApplication {
 
